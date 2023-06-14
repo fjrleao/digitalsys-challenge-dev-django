299998e0cfb619918cdd22e7840964ffec448773
Fábio Júnior <fjrleao@gmail.com>
2023-06-14 11:12:53 -0300

    fix: cpf unique field

--- backend/person/models.py
+++ backend/person/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=64)
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
     address = models.CharField(max_length=120)
 
     def __str__(self) -> str:
