88135d2c8c59e6d1efbb87474a3e2608d6842fd2
Fábio Júnior <fjrleao@gmail.com>
2023-06-14 10:47:42 -0300

    feat: start person app and create person model

---
+++ backend/person/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=64)
+    cpf = models.CharField(max_length=11)
+    address = models.CharField(max_length=120)
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.cpf}'
